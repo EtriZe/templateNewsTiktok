97df54aa079854a9f9cb01cbd1a7603f73f32bac
Valentin <valentin.barrerepro@gmail.com>
2025-06-07 17:05:24 +0200

    Update template

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-document.addEventListener("click", function(){
-    var news = document.querySelector(".carousel").children;
+onload = (event) => {
 
-    for (const [key, value] of Object.entries(news)) {
-        if(value.id === "current"){
-            value.id = "hide";
-            news[parseInt(key)+1].id = "current";
-            news[parseInt(key)+2].id = "next";            
+    var btnDown = document.querySelector(".downCarousel");
+    var btnReveal = document.querySelector(".revealInfos");
+
+    btnDown.addEventListener("click", function(){
+        var news = document.querySelector(".carousel_down").children;
+
+        for (const [key, value] of Object.entries(news)) {
+            if(value.id === "current"){
+                value.id = "hide";
+                value.classList.add("seen");
+                news[parseInt(key)+1].id = "current";
+                news[parseInt(key)+2].id = "next";            
+                break;
+            }
+        }
+
+    })
+
+    btnReveal.addEventListener("click", function(){
+        var news = document.querySelector(".carousel_down").children;
+
+        for (const [key, value] of Object.entries(news)) {
+            if(value.classList.contains("seen")) continue;
+
+            var details = value.querySelectorAll(".detail");
+            for (const [key, detail] of Object.entries(details)) {
+                if(detail.id === "hide") {
+                    detail.id = ""; 
+                    break;
+                }
+            }
             break;
         }
-    }
 
-})
+    })
+
+
+}
+
+
+
+
+
+
+
+
